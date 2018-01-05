308538b344164786e599fb02abdacce4376f9306
Bdita <fpbandita@gmail.com>
2018-01-06 03:01:05 +1100

    static files

--- cwmbackend/urls.py
+++ cwmbackend/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
 
@@ -21,3 +22,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('bookings.api.urls', namespace='api'))
 ]
+urlpatterns += staticfiles_urlpatterns()
