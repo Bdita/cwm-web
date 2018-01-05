3c321009c2fe88b3fca03429070e0a8010a155e5
Bdita <fpbandita@gmail.com>
2018-01-06 01:12:46 +1100

    production update

--- cwmbackend/settings/base.py
+++ cwmbackend/settings/base.py
@@ -6,8 +6,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
 # Application definition
 
 INSTALLED_APPS = [
