4ffaf967140c4a2621f84e2be1d891acb7e307fb
Bdita <fpbandita@gmail.com>
2018-01-07 01:35:43 +1100

    cors headers update

--- cwmbackend/settings/production.py
+++ cwmbackend/settings/production.py
@@ -5,10 +5,8 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# once frontend is deployed
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = []
 
-# what to do with production database
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
