2d343409fbd2c6798cc0f7d2ec71cbba94dc470e
Bdita <fpbandita@gmail.com>
2018-01-07 00:48:21 +1100

    django-cors-headers

--- cwmbackend/settings/base.py
+++ cwmbackend/settings/base.py
@@ -24,12 +24,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gunicorn',
-    'bookings'
+    'bookings',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
